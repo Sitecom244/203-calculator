e847e8f1bc9df4d17c04fc0f94dd961fc58cb5af
Sitecom244 <jeroen@specishops.nl>
2024-09-18 12:59:54 +0200

    Update fefco-203-calculator.js

--- fefco-203-calculator.js
+++ fefco-203-calculator.js
@@ -68,15 +68,15 @@ function calcformCalculate() {
     }
 
     if (!(validateNumber(length) && validateNumber(width) && validateNumber(height) && validateNumber(quantity))) {
-        AlertMSG('Please enter valid numbers for all dimensions and quantity.');
+        AlertMSG('Voer geldige cijfers in voor alle afmetingen en aantallen.');
         return;
     }
-    if (quantity < 1 || quantity >= 1001) {
-        AlertMSG('The price for less than 1 or 1000 or more boxes cannot be calculated online. Please request a quote.');
+    if (quantity < 10 || quantity >= 1001) {
+        AlertMSG('De prijs voor minder dan 10 of meer dan 1.000 dozen kunnen wij niet online berekenen. Neem contact met ons op om hier een offerte voor te ontvangen.');
         return;
     }
     if (((height + (width * 2) + 25) > 1800)) {
-        AlertMSG('Please request a quote.');
+        AlertMSG('Voor deze afmetingen kunnen wij niet direct een prijs berkenen. Neem contact met ons op om hier een offerte voor te ontvangen.');
         return;
     }
 
